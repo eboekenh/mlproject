77bbd07dc9aed5591143b8d3f20c5d7b01623a73
Ecem Bokenheide <ecemboekenheide@gmail.com>
2026-03-11 14:36:44 +0100

    data transformation updated

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -23,11 +23,11 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("Entered the data ingestion method or component")
         try:
-            df = pd.read_csv('notebook\data\stud.csv')
+            df = pd.read_csv(os.path.join('src', 'notebook', 'data', 'stud.csv'))
             logging.info("Read the dataset as dataframe")
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
-            
+
             df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
 
             logging.info("Train test split initiated")
